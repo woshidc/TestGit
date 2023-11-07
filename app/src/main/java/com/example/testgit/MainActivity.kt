c2588f9cbaf3a9bee329f01bfb9a0d304eee9546
dacheng <190253475@qq.com>
2023-11-07 21:19:01 +0800

    implement: buttonAdd onClick
    ###
    实现了+Button的功能
    ###

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.testgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var num = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textview.text="${num}"
+        button.setOnClickListener {
+            textview.text="${++num}"
+        }
+
+
     }
 }
